62d9575a065de47584046ba5b39816dd9615544c
Cristian <astudillo82@gmail.com>
2020-04-06 13:14:43 +0200

    main-page style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
 
 import * as firebase from 'firebase/app';
 import firebaseConfig from './firebaseConfig';
